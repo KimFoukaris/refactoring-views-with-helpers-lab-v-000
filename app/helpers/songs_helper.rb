37c6d015a86a62acfd64174aff14bb52f66a6142
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-24 10:17:50 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,10 +1,9 @@
 module SongsHelper
   def display_artist(song)
     if song.artist.exist?
-      @artist = song.artist
-      link_to song.artist.name, artist_path(@artist)
+      link_to song.artist.name, artist_path(song.artist)
     else
-      link_to song.title, edit_song_path(song)
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 end
