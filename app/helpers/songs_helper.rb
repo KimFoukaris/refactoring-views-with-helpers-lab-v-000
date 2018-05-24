b43598e7f8080dcc6109e671ab5380a186ff138a
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-24 10:03:11 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,7 +3,7 @@ module SongsHelper
     if song.artist.exist?
       link_to song.artist.name, artist_path
     else
-      link_to song.name, edit_song_path
+      link_to song.title, edit_song_path
     end
   end
 end
